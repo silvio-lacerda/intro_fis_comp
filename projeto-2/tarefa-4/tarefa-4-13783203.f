d8c916c3e51ae1d561f3bc28f30884fe1179b21e
Silvio Lacerda <silviolacerda21@hotmail.com>
2023-09-24 22:15:19 -0300

    feat

--- projeto-2/tarefa-4/tarefa-4-13783203.f
+++ projeto-2/tarefa-4/tarefa-4-13783203.f
@@ -3,45 +3,46 @@
         parameter (n_passos=1000)
         dimension vec_x(n_andarilhos)
         dimension vec_y(n_andarilhos)
-        dimension i_r(2)
-        dimension i_path(2)
-
+        
+        p_walk = 1.e0/4.e0
         vec_x = 0
         vec_y = 0
-
-        p_walk = 1.e0/4.e0
-
-        aMedia = 0.0e0
-        aMediaQ = 0.0e0
-
-        do j=1, n_andarilhos
-          i_r = 0
-
-          do i=1, n_passos
+        i_s = 50
+        
+        open(unit=1, file="saida-4-13783203.dat")
+        do m=1, n_passos
+          do n=1, n_andarilhos
             prob = rand()
             if (prob .LE. p_walk) then
-              i_r(1) = i_r(1) - 1
+              vec_x(n) = vec_x(n) - 1
             else if (prob .GT. p_walk .AND. prob .LE. p_walk*2) then
-              i_r(1) = i_r(1) + 1
+              vec_x(n) = vec_x(n) + 1
             else if (prob .GT. p_walk*2 .AND. prob .LE. p_walk*3) then
-              i_r(2) = i_r(2) - 1
+              vec_y(n) = vec_y(n) - 1
             else
-              i_r(2) = i_r(2) + 1
+              vec_y(n) = vec_y(n) + 1
             end if
           end do
 
-          r = SQRT(real(i_r(1))**2 + real(i_r(2))**2)
-          aMedia = aMedia + r
-          aMediaQ = aMediaQ + r**2
+          S = 0
+          do i=-n_passos, n_passos, i_s
+            do j=-n_passos, n_passos, i_s
+              n_inside = 0
+              do k=1, n_andarilhos
+                if (vec_x(k) .GT. i .AND. vec_x(k) .LT. i+i_s) then
+                  if (vec_y(k) .GT. j .AND. vec_y(k) .LT. j+i_s) then
+                    n_inside = n_inside + 1
+                  end if
+                end if
+              end do
 
-          vec_x(j) = i_r(1)
-          vec_y(j) = i_r(2)
+              p = real(n_inside)/real(n_andarilhos)
+              if (p .GT. 0.0e0) then
+                S = S - (p * log(p))
+              end if
+            end do
+          end do
+          write(1,*) S
         end do
-
-        i_quadrado = 2
-        i_qtd_quadrados = n_passos // i_quadrado
-        
-        do i=-i_qtd_quadrados, i_qtd_quadrados
-         
-        end do
+        close(unit=1)
         end program
